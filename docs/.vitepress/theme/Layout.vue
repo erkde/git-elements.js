ad8b9f3710e26ecf7cd7457a6e8520f121583864
Erik Eide <351763+erkde@users.noreply.github.com>
2026-09-19 19:30:11 +1000

    Remove doc hero animation

--- docs/.vitepress/theme/Layout.vue
+++ docs/.vitepress/theme/Layout.vue
@@ -1,12 +1,7 @@
 <script setup lang="ts">
 import DefaultTheme from "vitepress/theme";
-import HeroGitLog from "./components/HeroGitLog.vue";
 </script>
 
 <template>
-  <DefaultTheme.Layout>
-    <template #home-hero-image>
-      <HeroGitLog />
-    </template>
-  </DefaultTheme.Layout>
+  <DefaultTheme.Layout />
 </template>
